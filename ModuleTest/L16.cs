0d571cfb0d5ccb44f69506f15260c72c8dfecd04
tretyakovSi <tretyakovSi@sber-service.ru>
2025-03-11 22:07:21 +0300

    LR16

---
+++ ModuleTest/L16.cs
@@ -0,0 +1,77 @@
+﻿using Moq;
+using SpaceBattle;
+using SpaceBattle.Commands;
+using SpaceBattle.Interfaces;
+using SpaceBattle.IoC;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace ModuleTest
+{
+    [TestClass]
+    public class L16
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            new InitScopesCommand().Execute();
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            var mock = new Mock<ServerThreadStartCommand>();
+            var cmd = mock.Object;
+
+            cmd.Execute();
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            BlockingCollection<ICommand> q = new BlockingCollection<ICommand>(100);
+            var st = new ServerThread(q);
+
+            var cmd = new Mock<ICommand>();
+
+            q.Add(cmd.Object);
+            q.Add(cmd.Object);
+            q.Add(new HardStopCommand(st));
+            q.Add(cmd.Object);
+            q.Add(cmd.Object);
+
+            var result = false;
+
+            st.SetActionAfterStop(() => {                
+                result = true;
+            });
+            st.Start();
+            st.Join();
+
+
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            BlockingCollection<ICommand> q = new BlockingCollection<ICommand>(100);
+            var st = new ServerThread(q);
+
+            var cmd = new Mock<ICommand>();
+
+            q.Add(cmd.Object);
+            q.Add(cmd.Object);
+            q.Add(new SoftStopCommand(st));
+            q.Add(cmd.Object);
+            q.Add(cmd.Object);
+
+            st.Start();
+            st.Join();
+
+            Assert.AreEqual(st._q.Count, 0);
+        }
+    }
+}
---
+++ SpaceBattle/Commands/SoftStopCommand .cs
@@ -0,0 +1,28 @@
+﻿using SpaceBattle.Interfaces;
+
+namespace SpaceBattle.Commands
+{
+    public class SoftStopCommand : ICommand
+    {
+        private ServerThread _st;
+        public SoftStopCommand(ServerThread st)
+        {
+            _st = st;
+        }
+
+        public void Execute()
+        {
+            Action oldBehaviour = _st._behaviour;
+            _st.SetBehaviour(() => {                
+                if (_st._q.Count > 0)
+                {
+                    oldBehaviour();
+                }
+                else
+                {
+                    _st.Stop();
+                }
+            });
+        }
+    }
+}
